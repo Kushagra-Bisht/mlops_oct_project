4566a72fcfb9191f8cd05545443a127e79168118
Your Name <bishtkushagra28@gmail.com>
2024-10-17 06:12:25 +0530

    add docker steps to ci

--- notebooks/dags.py
+++ notebooks/dags.py
@@ -8,3 +8,5 @@ dagshub.init(repo_owner='Kushagra-Bisht', repo_name='mlops_oct_project', mlflow=
 with mlflow.start_run():
   mlflow.log_param('parameter name', 'value')
   mlflow.log_metric('metric name', 1)
+
+#token =b7cdd6a7f1c7cfd93d88dfbbc0322e6084f18a45
